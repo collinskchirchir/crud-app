91a7a5494677209551bdec314c00c86c2ee4601c
Collins Chirchir <collinskchirchir@gmail.com>
2023-06-20 12:52:40 +0300

    employee model

---
+++ backend/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace backend.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public string? Age { get; set; }
+        public int IsActive { get; set; }
+    }
+}
