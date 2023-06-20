5616762bc996a1e7ced97b0e5e87c559c65bc76b
Collins Chirchir <collinskchirchir@gmail.com>
2023-06-20 13:09:43 +0300

    db context

---
+++ backend/Models/EmployeeContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace backend.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options) { 
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+        
+        
+            
+        
+    }
+}
